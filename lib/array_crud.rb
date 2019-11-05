373419ae6a9c6ae29df0f1fa290c5d8a23725781
Henry Phan <h.phan06@gmail.com>
2019-11-05 03:04:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  array = [0, 1, 2, 3]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
